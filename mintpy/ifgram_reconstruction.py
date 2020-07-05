ebe17194819f08e72449ab80fd9c32bf42a0b6c2
Yunmeng Cao <ymcmrs@gmail.com>
2020-07-05 22:35:51 +0300

    Add files via upload

--- mintpy/ifgram_reconstruction.py
+++ mintpy/ifgram_reconstruction.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 ############################################################
 # Program is part of MintPy                                #
-# Copyright(c) 2013-2019, Zhang Yunjun, Heresh Fattahi     #
-# Author:  Zhang Yunjun, Heresh Fattahi, Yunmeng Cao       #
+# Copyright (c) 2013, Zhang Yunjun, Heresh Fattahi         #
+# Author: Zhang Yunjun, Heresh Fattahi, 2013               #
 ############################################################
-
-
+import os
+import h5py
 import argparse
 import numpy as np
 from mintpy.objects import ifgramStack
 from mintpy.utils import readfile, writefile
 
-# Modify 2019-07-09 By Yunmeng Cao
-# reference reconstruct consider the dropifgrams in the original ifgram_file
+def write_h5(datasetDict, out_file, metadata=None, ref_file=None, compression=None):
+
+    if os.path.isfile(out_file):
+        print('delete exsited file: {}'.format(out_file))
+        os.remove(out_file)
+
+    print('create HDF5 file: {} with w mode'.format(out_file))
+    with h5py.File(out_file, 'w') as f:
+        for dsName in datasetDict.keys():
+            data = datasetDict[dsName]
+            ds = f.create_dataset(dsName,
+                              data=data,
+                              compression=compression)
+
+        for key, value in metadata.items():
+            f.attrs[key] = str(value)
+            #print(key + ': ' +  value)
+    print('finished writing to {}'.format(out_file))
+
+    return out_file
 
 #####################################################################################
 EXAMPLE = """example:
@@ -42,6 +60,7 @@ def cmd_line_parse(iargs=None):
 def timeseries2ifgram(ts_file, ifgram_file, out_file='reconUnwrapIfgram.h5'):
     # read time-series
     atr = readfile.read_attribute(ts_file)
+    atr0 = readfile.read_attribute(ifgram_file)
     range2phase = -4.*np.pi / float(atr['WAVELENGTH'])
     print('reading timeseries data from file {} ...'.format(ts_file))
     ts_data = readfile.read(ts_file)[0] * range2phase
@@ -51,17 +70,16 @@ def timeseries2ifgram(ts_file, ifgram_file, out_file='reconUnwrapIfgram.h5'):
     # reconstruct unwrapPhase
     print('reconstructing the interferograms from timeseries')
     stack_obj = ifgramStack(ifgram_file)
-    stack_obj.open(print_msg=False)  
-    date12 = readfile.read(ifgram_file, datasetName='date')[0]
-    bperp = readfile.read(ifgram_file, datasetName='bperp')[0]
-    droplist = readfile.read(ifgram_file, datasetName='dropIfgram')[0]
-    date = stack_obj.get_date_list(dropIfgram=True)
-    date = np.asarray(date,dtype='|S8')
-    print(date)
-    meta = readfile.read_attribute(ifgram_file,datasetName=None)
-    
-    #A1 = stack_obj.get_design_matrix4timeseries(stack_obj.get_date12_list(dropIfgram=False))[0]
+    stack_obj.open(print_msg=False)
     A1 = stack_obj.get_design_matrix4timeseries(stack_obj.get_date12_list(dropIfgram=True))[0]
+    date_list = stack_obj.get_date_list(dropIfgram=True)
+    date12 = stack_obj.get_date12_list(dropIfgram=True)
+    print(date12)
+   
+    with h5py.File(ifgram_file, 'r') as f:
+        pbaseIfgram = f['bperp'][:]
+        pbaseIfgram = pbaseIfgram[f['dropIfgram'][:]]
+
     num_ifgram = A1.shape[0]
     A0 = -1.*np.ones((num_ifgram, 1))
     A = np.hstack((A0, A1))
@@ -69,16 +87,24 @@ def timeseries2ifgram(ts_file, ifgram_file, out_file='reconUnwrapIfgram.h5'):
     ifgram_est = np.array(ifgram_est, dtype=ts_data.dtype)
     del ts_data
     
-    
+    date12m = np.zeros((len(date12),2),dtype = '<S8')
+    for i in range(len(date12)):
+        k0 = date12[i]
+        date12m[i,0]=k0.split('_')[0] 
+        date12m[i,1] = k0.split('_')[1]
+
+    dropIfgram = np.zeros((len(date12),),dtype = bool) 
+    print(np.asarray(date12m,dtype = '<S8'))
     # write to ifgram file
     dsDict = {}
     dsDict['unwrapPhase'] = ifgram_est
-    dsDict['date'] = date12[droplist]
-    dsDict['date_list'] = date
-    dsDict['bperp'] = bperp[droplist]
-    dsDict['dropIfgram'] = droplist[droplist]
+    dsDict['dropIfgram'] = dropIfgram
+    dsDict['date_list'] = np.asarray(date_list,dtype = '<S8')
+    dsDict['date'] = np.asarray(date12m,dtype = '<S8')
+    dsDict['bperp'] = np.asarray(pbaseIfgram)
+    write_h5(dsDict, out_file = out_file, metadata=atr0, ref_file=None, compression=None)
     
-    writefile.write(dsDict, out_file=out_file, metadata=meta)
+    #writefile.write(dsDict, out_file=out_file, ref_file=ifgram_file)
     return ifgram_file
 
 
